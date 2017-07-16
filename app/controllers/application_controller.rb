cacd895d87f1a60d2c1912326ba88b6e961b9758
Andre LeBlanc <andre.leblanc@accelerated.com>
2017-07-16 18:40:31 -0400

    add sentry

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,7 +17,7 @@ class ApplicationController < ActionController::Base
   end
 
   def set_raven_context
-    Raven.user_context(id: current_user.id)
+    Raven.user_context(id: current_user.id) if current_user.present?
     Raven.extra_context(params: params.to_unsafe_h, url: request.url)
   end
 
